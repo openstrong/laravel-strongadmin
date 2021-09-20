29bd6e0b97bca34366174ddb8100229240bbe9f8
Karen <openstrong@foxmail.com>
2021-09-20 15:56:34 +0800

    验证码

--- src/Http/Controllers/AdminAuthController.php
+++ src/Http/Controllers/AdminAuthController.php
@@ -19,9 +19,7 @@ class AdminAuthController extends BaseController
 
     public function __construct()
     {
-        //允许尝试登录最大次数
         $this->maxAttempts = config('strongadmin.storage.throttles_logins.maxAttempts', 5);
-        //登录错误超过 maxAttempts 次, 禁止登录 decayMinutes 分钟
         $this->decayMinutes = config('strongadmin.storage.throttles_logins.decayMinutes', 10);
     }
 
